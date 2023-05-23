765957d0091695a9b2661bc5594d25f1b91bf27e
minami murata <minami_murata@outlook.com>
2023-05-23 21:50:17 +0900

    commit contact_app

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,23 @@
+class ContactsController < ApplicationController
+  # def controller
+  # end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "done!!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :content)
+  end
+end
